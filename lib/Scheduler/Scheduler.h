58a985bee1005257f1ded3b19451d42ebc2b11fe
as3ii <as3ii777@gmail.com>
2023-11-15 11:01:51 +0100

    fixed formatting of lib/Scheduler classes

--- lib/Scheduler/Scheduler.h
+++ lib/Scheduler/Scheduler.h
@@ -1,21 +1,22 @@
 #ifndef __SCHEDULER__
 #define __SCHEDULER__
+
 #include "Timer.h"
 #include "Task.h"
+
 #define MAX_TASKS 10
 
 class Scheduler {
+    private:
+        int basePeriod;
+        int nTasks;
+        Task* taskList[MAX_TASKS];
+        Timer timer;
 
-int basePeriod;
-int nTasks;
-Task* taskList[MAX_TASKS];
-Timer timer;
-public:
-
-void init(int basePeriod);
-virtual bool addTask(Task* task);
-virtual void schedule();
-
+    public:
+        void init(int basePeriod);
+        virtual bool addTask(Task* task);
+        virtual void schedule();
 };
 
 #endif
--- lib/Scheduler/Task.h
+++ lib/Scheduler/Task.h
@@ -1,25 +1,28 @@
 #ifndef __TASK__
 #define __TASK__
-class Task {
 
-    int myPeriod;
-    int timeElapsed;
+class Task {
+    private:
+        int myPeriod;
+        int timeElapsed;
 
     public:
-    virtual void init(int period){
-    myPeriod = period;
-    timeElapsed = 0;
-    }
-        
-    virtual void tick() = 0;
-    bool updateAndCheckTime(int basePeriod){
-        timeElapsed += basePeriod;
-        if (timeElapsed >= myPeriod){
+        virtual void init(int period) {
+            myPeriod = period;
             timeElapsed = 0;
-        return true;
+        }
+
+    virtual void tick() = 0;
+
+    bool updateAndCheckTime(int basePeriod) {
+        timeElapsed += basePeriod;
+        if (timeElapsed >= myPeriod) {
+            timeElapsed = 0;
+            return true;
         } else {
             return false;
         }
     }
 };
+
 #endif
