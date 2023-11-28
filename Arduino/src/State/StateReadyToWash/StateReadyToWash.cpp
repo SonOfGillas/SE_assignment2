bc23377b14e22ada42fcc0945ff46d1969a328c1
as3ii <as3ii777@gmail.com>
2023-11-28 23:27:28 +0100

    added first implementation of StateReadyToWash

---
+++ Arduino/src/State/StateReadyToWash/StateReadyToWash.cpp
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+#include <Scheduler.h>
+#include <Config.h>
+#include "StateReadyToWash.h"
+#include "Components/Components.h"
+#include "Tasks/WatchButton/WatchButton.h"
+
+StateName StateReadyToWash::name() {
+    return StateName::EnteringWashingArea;
+}
+
+StateReadyToWash::StateReadyToWash(int carWashed, Components* components, Scheduler* scheduler): State(carWashed) {
+    this->components = components;
+    this->scheduler = scheduler;
+
+    this->isButtonPressed = false;
+
+    // close gate
+    this->components->gate->setPosition(0);
+
+    // switch on L2
+    this->components->l2->switchOn();
+
+    // print to the screen
+    this->components->userLcd->clear();
+    this->components->userLcd->setCursor(0, 0); 
+    this->components->userLcd->print("Ready to wash");
+    
+    Task* watchButton = new WatchButton(components, this);
+    watchButton->init(100);
+    this->scheduler->addTask(watchButton);
+}
+
+bool StateReadyToWash::goNext() {
+    return this->isButtonPressed;
+}
+
+void StateReadyToWash::buttonPressed(bool isButtonPressed) {
+    if(isButtonPressed && !this->isButtonPressed)
+        this->isButtonPressed = true;
+}
+
+StateReadyToWash::~StateReadyToWash() {
+    scheduler->removeLastTask(); // remove watch task
+}
---
+++ Arduino/src/State/StateReadyToWash/StateReadyToWash.h
@@ -0,0 +1,24 @@
+#ifndef __STATE_READY_TO_WASH__
+#define __STATE_READY_TO_WASH__
+
+#include <Scheduler.h>
+#include "State/State.h"
+#include "Components/Components.h"
+#include "Tasks/WatchButton/WatchButton.h"
+#include "Tasks/WatchButton/ButtonListener.h"
+
+class StateReadyToWash: public State, public ButtonListener {
+    private:
+        Components* components;
+        Scheduler* scheduler;
+        bool isButtonPressed;
+        long enteredTime;
+    public:
+        StateReadyToWash(int carWashed, Components* components, Scheduler* scheduler);
+        ~StateReadyToWash();
+        StateName name();
+        void buttonPressed(bool isButtonPressed);
+        bool goNext();
+};
+
+#endif
